8dd9516b717d53d4b49daeb38392147392551164
nvlmlna <mnaufalmaulana4629@gmail.com>
2025-06-01 02:08:21 +0700

    Biru Laut

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,11 +3,15 @@ export default function MainContent () {
 <main className="flex-grow p-6 bg-gray-300 dark:bg-gray-800 overflow-auto min-h-screen">
       <div className="w-full text-right px-8">
         <h1 className="text-6xl md:text-7xl font-extrabold text-gray-800 dark:text-white leading-tight">
-            Cold,<br />
-            Smooth<br />
-            & Cruel.
+            Blue<br />
+            Ocean<br />
+            News
           </h1>
-          <p className="text-sm font-semibold mt-6 text-gray-700 tracking-wider">AZFDFKHZ</p>
+          <p className="text-sm font-semibold mt-6 text-gray-700 tracking-wider">
+            Matilah engkau mati<br />
+            kau akan lahir berkali-kali<br />
+            -Sang Penyair-
+            </p>
         </div>
       </main>
     );
--- src/data/posts-IOT.js
+++ src/data/posts-IOT.js
@@ -2,10 +2,10 @@ import paths from "../assets/paths-niki.png";
 export const posts = [
   {
     id: 1,
-    title: "How to use React",
-    excerpt: "Learn the basics of React in this introductory post.",
-    content: "This is the full content of the React blog post.",
-    date: "2025-05-15",
+    title: "Naufal And Shalsa",
+    excerpt: "How can i move on when im still in love with you.",
+    content: "How to make her mine.",
+    date: "2024-08-12",
     imageUrl: paths,
   },
   {
